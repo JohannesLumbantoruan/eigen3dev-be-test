7c29d73a1d2fc5f58cc98b6f3c7c034217644e98
Johannes Lumbantoruan <johannestrikardo@gmail.com>
2024-02-28 16:24:52 +0700

    Create documentation for get /books

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,3 +1,49 @@
+/**
+ * @openapi
+ * components:
+ *  schemas:
+ *    Books:
+ *      type: object
+ *      properties:
+ *        success:
+ *          type: boolean
+ *          example: true
+ *        books:
+ *          type: array
+ *          items:
+ *            type: object
+ *            properties:
+ *              code:
+ *                type: string
+ *                example: book-12345
+ *              title:
+ *                type: string
+ *                example: Twilight
+ *              author:
+ *                type: string
+ *                example: Stephenie Meyer
+ *              stock:
+ *                type: integer
+ *                format: int64
+ *                example: 5
+ * tags:
+ *  - name: books
+ *    description: All operations for path books
+ * /books:
+ *  get:
+ *    tags:
+ *      - books
+ *    summary: Get list of the books
+ *    description: Get all books and its detail
+ *    responses:
+ *      200:
+ *        description: Get all books
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Books'
+ */
+
 const express = require("express");
 
 const authenticate = require("../middlewares/authenticate");
